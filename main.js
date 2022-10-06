5083e8694bcb258b7ca2ec0dd0fcb15e262e7e22
sardor <@23.ru>
2022-10-06 18:06:31 +0500

    first

---
+++ main.js
@@ -0,0 +1,15 @@
+
+var _name = prompt('Введите своё имя');
+console.log('Меня зовут '+ _name);
+var age = +prompt('Введите свой возраст');
+console.log('Мне ' + age);
+var primer1 = +prompt('Решите пример' +'12+3=?')
+console.log('Пример 1: ' + '12+3='+ (12+3)+' Ваш ответ: ' + primer1);
+var primer2 = +prompt('Решите пример' +'10-3=?')
+console.log('Пример 2: ' + '12-3='+ (12-3)+' Ваш ответ: ' + primer2);
+var primer3 = +prompt('Решите пример' +'14:2=?')
+console.log('Пример 3: ' + '14:2='+ 14/2+' Ваш ответ: ' + primer3);
+var primer4 = +prompt('Решите пример' +'5*6=?')
+console.log('Пример 4: ' + '5*6='+ 5*6 +' Ваш ответ: ' + primer4);
+var primer5 = +prompt('Решите пример' +'10%6=?')
+console.log('Пример 5: ' + '10%6='+ 10%6 +' Ваш ответ: ' + primer5);
